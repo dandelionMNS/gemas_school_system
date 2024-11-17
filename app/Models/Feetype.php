1a19438b3c046dbcf0e06787e6e6baa1f319162f
Dandelion Coder <134459077+dandelionMNS@users.noreply.github.com>
2024-11-17 22:36:20 +0800

    Modify: Fix Feetype

--- app/Models/Feetype.php
+++ app/Models/Feetype.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Feetypes extends Model
+class Feetype extends Model
 {
     use HasFactory;
 
